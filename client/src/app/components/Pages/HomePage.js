b3fd606ff35e875c136e3e3d41c75b5ed95b6477
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-11-04 20:51:00 +0200

    test

--- client/src/app/components/Pages/HomePage.js
+++ client/src/app/components/Pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default ({ className }) => {
-    return (<div className={className}><h1> home page</h1></div>);
+    return (<div className={className}><h1>home page</h1></div>);
 };
