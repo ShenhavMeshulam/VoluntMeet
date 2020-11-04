bc37fc8645f01201d362ee30eb00cf3fdb00f5e3
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-11-04 23:04:41 +0200

    feat(Event): create event model

---
+++ server/src/models/Event.js
@@ -0,0 +1,53 @@
+import {Schema, model as createModel} from 'mongoose';
+
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  creationDate: {
+    type: Date,
+    default: () => new Date()
+  }
+});
+
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+    required: false
+  },
+  minPeople: {
+    type: Number,
+    required: true
+  },
+  maxPeople: {
+    type: Number,
+    required: true
+  },
+  comments: {
+    type: [commentSchema],
+    required: false
+  }
+});
+
+export const Event = createModel('Event', schema);
