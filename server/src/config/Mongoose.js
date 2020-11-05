81d6db9ed6bc9fab1799a7123e4d8152003549c1
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-11-05 14:14:28 +0200

    feat(mongoose): set default mongo to compass

--- server/src/config/Mongoose.js
+++ server/src/config/Mongoose.js
@@ -1,4 +1,4 @@
 import {connect} from 'mongoose';
 
 export const configureMongoose = () =>
-  connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  connect(process.env.MONGO_URL ?? 'mongodb+srv://root:xz6F9V46jwU4i3s@prod.rj7wa.mongodb.net/VoluntMeetDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
